59e6aa5712af484cd1088a628e2515b7ac3aeb01
Kapil <kapily1011@gmail.com>
2025-12-27 20:53:44 +0530

    completed the snake movement

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
 const board = document.querySelector('.boards');  
-const blockHeight = 80;
-const blockWidth = 80;
+const blockHeight = 50 ;
+const blockWidth = 50 ;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+
+const blocks = [];
+const snake = [
+    { x:1, y:3},// jo pehla object rahega vo hee snake ka head rahega
+    { x:1, y:4},
+    { x:1, y:5}, 
+]
+
+let direction = 'right'
 
 for(let row = 0; row < rows; row++) {
     for(let col = 0; col < cols; col++) {
         const block = document.createElement('div');
         block.classList.add('block');
-        board.appendChild(block);  
-        block.innerText = `${row}-${col}`
+        board.appendChild(block);   
+        blocks[ `${row}-${col}` ] = block
     }
 }
+
+function render(){
+    snake.forEach(segment =>{
+        blocks[ `${segment.x}-${segment.y}`].classList.add("fill")//snake ko aage move karega   
+    })
+}
+
+setInterval(()=>{
+    let head = null;
+    if(direction =="left"){
+        head = { x: snake[0].x, y:snake[0].y - 1}
+    } else if(direction =="right"){
+        head = { x: snake[0].x, y:snake[0].y + 1}
+    } else if(direction =="down"){
+        head = { x: snake[0].x+1, y:snake[0].y}
+    } else if(direction =="up"){
+        head = { x:snake[0].x-1, y:snake[0].y}
+    }
+
+    snake.forEach(segment=>{
+        blocks[ `${segment.x}-${segment.y}`].classList.remove("fill")// snake ka back part ko remove karega 
+    })
+
+    snake.unshift(head)
+    snake.pop()
+
+    render()
+}, 400); 
+
+addEventListener("keydown", (event) => {
+    if(event.key == "ArrowUp"){
+        direction = "up"
+    } else if(event.key == "ArrowRight"){
+        direction = "right"
+    } else if(event.key == "ArrowLeft"){
+        direction = "left"
+    } else if(event.key == "ArrowDown"){
+        direction = "down"
+    }
+})
+ 
+
+
